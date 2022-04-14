27648741ff5be20eb5c73a4a7a1a6ff3451a7abc
Vidar Eldøy <vidar@eldoy.com>
2022-04-14 05:04:40 +0200

    Shorten tester

--- spec/lib/test.js
+++ spec/lib/test.js
@@ -3,11 +3,10 @@ const request = require('../lib/request.js')
 
 module.exports = async function test(name) {
   try {
-    const result = await require(`../tests/${name}.test.js`)({
+    return await require(`../tests/${name}.test.js`)({
       t: assert,
       r: request
     })
-    return result
   } catch (e) {
     console.log(`\n* Test "${name}" failed:\n`)
     console.log(e.message)
