d5d2c27998783750b8b871a921e492401e9af60c
Vidar Eldøy <vidar@eldoy.com>
2022-04-20 15:52:40 +0200

    Rearrange functions

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ console.log({ mode })
 
 const root = process.cwd()
 
-// Scan assets
-function scanAssets(dir) {
-  if (typeof dir != 'string') return
-  const assetPath = path.join(root, dir)
-  const assetList = extras.tree(assetPath).map(x => x.replace(assetPath, ''))
-  return new Set(assetList)
-}
-
 function setContentType(res, type) {
   if (!res.hasHeader('content-type')) {
     res.setHeader('content-type', `${type}; charset=utf-8`)
   }
+}
+
+// Make asset list
+function getAssets(dir) {
+  if (typeof dir != 'string') return
+  const assetPath = path.join(root, dir)
+  const assetList = extras.tree(assetPath).map(x => x.replace(assetPath, ''))
+  return new Set(assetList)
 }
 
 function favicon(res) {
@@ -55,7 +55,87 @@ function setFile(req) {
   }
 }
 
-function handleResult(req, res, result) {
+module.exports = function(opt, fn) {
+  if (typeof opt == 'function') {
+    opt = {}
+    fn = opt
+  }
+
+  // Store assets as a set
+  const assets = getAssets(opt.assets)
+
+  const server = http.createServer(function(req, res) {
+    // Add request properties
+    rekvest(req)
+
+    // Return asset if it exists
+    if (assets && assets.has(req.pathname)) {
+      return smor(req, res, { dir: 'app/assets' })
+    }
+
+    // Block favicon requests
+    if (req.pathname == '/favicon.ico') {
+      return favicon(res)
+    }
+
+    // Cors
+    if (req.method == 'OPTIONS') {
+      return cors(req, res)
+    }
+
+    handleRequest(req, res, opt, fn)
+  })
+
+  // Set up web socket for development
+  if (mode == 'development') {
+    new ws.Server({ server })
+  }
+
+  server.listen(opt.port || 9090, () => {
+    const port = server.address().port
+    console.log('Web server is listening on port %d', port)
+  })
+
+  return server
+}
+
+async function handleRequest(req, res, opt, fn) {
+  console.log({ path: req.pathname })
+
+  // Routes
+  if (opt.routes) {
+    router(req, opt.routes)
+  }
+
+  // Set up cookies
+  wcookie(req)
+
+  // Set up lang
+  lang(req, opt.lang)
+
+  // Set file
+  setFile(req)
+
+  if (req.method == 'GET') {
+
+    setContentType(res, 'text/html')
+
+  } else if (req.method == 'POST') {
+
+    // Parse body only if POST request
+    await bparse(req)
+
+    setContentType(res, 'application/json')
+
+  } else {
+    res.statusCode = 204
+  }
+
+  let result
+  if (typeof fn == 'function') {
+    result = await fn(req, res)
+  }
+
   // Undefined, null and 0 returns empty string
   if (!result) result = ''
 
@@ -80,84 +160,4 @@ function handleResult(req, res, result) {
   }
 
   res.end(result)
-}
-
-async function handleRequest(req, res, fn) {
-  console.log({ path: req.pathname })
-
-  // Set up cookies
-  wcookie(req)
-
-  // Set up lang
-  lang(req)
-
-  if (req.method == 'GET') {
-    setFile(req)
-
-    setContentType(res, 'text/html')
-
-  } else if (req.method == 'POST') {
-
-    // Parse body only if POST request
-    await bparse(req)
-
-    setContentType(res, 'application/json')
-
-  } else {
-    res.statusCode = 204
-  }
-
-  let result
-  if (typeof fn == 'function') {
-    result = await fn(req, res)
-  }
-
-  // Handle the result
-  handleResult(req, res, result)
-}
-
-module.exports = function(opt, fn) {
-  if (typeof opt == 'function') {
-    opt = {}
-    fn = opt
-  }
-
-  // Store assets as a set
-  const assets = scanAssets(opt.assets)
-
-  const server = http.createServer(function(req, res) {
-    // Add request properties
-    rekvest(req)
-
-    // Return asset if it exists
-    if (assets && assets.has(req.pathname)) {
-      return smor(req, res, { dir: 'app/assets' })
-    }
-
-    // Block favicon requests
-    if (req.pathname == '/favicon.ico') {
-      return favicon(res)
-    }
-
-    // Cors
-    if (req.method == 'OPTIONS') {
-      return cors(req, res)
-    }
-
-    // Routes
-    if (opt.routes) {
-      router(req, opt.routes)
-    }
-
-    handleRequest(req, res, fn)
-  })
-
-  // Set up web socket for development
-  if (mode == 'development') {
-    new ws.Server({ server })
-  }
-
-  server.listen(opt.port || 9090)
-
-  return server
 }
